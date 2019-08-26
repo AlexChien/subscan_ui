8e8e93c33f262a97797749334bf12b19bc3eb7c2
Wangwenhan <wangwenhan4238@qq.com>
2019-08-26 16:44:17 +0800

    change publicPath

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ const productionGzipExtensions = ["js", "css"];
 
 module.exports = {
   outputDir: "web", // 当运行 vue-cli-service build 时生成的生产环境构建文件的目录。注意目标目录在构建之前会被清除 (构建时传入 --no-clean 可关闭该行为)
-  publicPath: process.env.NODE_ENV === "production" ? "/web/" : "/", // 部署应用包时的基本 URL。
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/", // 部署应用包时的基本 URL。
   // productionSourceMap: false,
   chainWebpack: config => {
     // 添加别名

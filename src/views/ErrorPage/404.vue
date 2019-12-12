3c8ff4931d2af7261fe5b87441c6094818974e29
Hongtao <acarlhong@gmail.com>
2019-12-12 14:31:35 +0800

    adjust theme

--- src/views/ErrorPage/404.vue
+++ src/views/ErrorPage/404.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="error-page-wrapper subscan-content">
     <div class="container">
-      <search-input
-        class="search-input"
-        :selectList="selectList"
-        :placeholder="$t('placeholder.search_by')"
-      />
       <div class="not-found">
         <img class="not-found-img" src="./../../assets/images/404.png" alt="404" />
         <div class="no-data">{{$t('error.404')}}</div>
@@ -15,11 +10,7 @@
 </template>
 
 <script>
-import SearchInput from "@/views/Components/SearchInput";
 export default {
-  components: {
-    SearchInput
-  },
   data() {
     return {
       selectList: [

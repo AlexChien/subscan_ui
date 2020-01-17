1d2befffd305a13a29b02c117118305f9dfe1646
Hongtao <acarlhong@gmail.com>
2020-01-17 16:58:51 +0800

    support edgeware

--- src/service/const/symbol.js
+++ src/service/const/symbol.js
@@ -1,6 +1,7 @@
 const ring_icon = require('./../../assets/images/ring@2x.png')
 const kton_icon = require('./../../assets/images/kton@2x.png')
 const ksm_icon = require('./../../assets/images/ksm@2x.png')
+const edg_icon = require('./../../assets/images/edgeware.png')
 
 export default [{
   name: 'icefrog',
@@ -19,6 +20,24 @@ export default [{
     },
     domain: {
       value: 'https://icefrog.subscan.io'
+    },
+    donate: {
+      address: '5DobnXBB9EU8gJR8ygCM5GvHHuvSRCfWypuC2vDqW35Udbku'
+    }
+  }
+}, {
+  name: 'edgeware',
+  value: {
+    balances: {
+      value: "testEDG",
+      icon: edg_icon
+    },
+    kton: {
+      value: "KTON",
+      icon: kton_icon
+    },
+    domain: {
+      value: 'https://edgeware.subscan.io/'
     },
     donate: {
       address: '5DobnXBB9EU8gJR8ygCM5GvHHuvSRCfWypuC2vDqW35Udbku'

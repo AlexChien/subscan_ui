7ce19489f326bb16095cf6d0371bfc500f31183c
Hongtao <acarlhong@gmail.com>
2020-01-19 14:17:05 +0800

    use cookie to store local_language

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,63 +1,89 @@
+import Cookies from 'js-cookie'
+
+/**
+ * 存储cookie
+ */
+export const setCookie = (name, value, options) => {
+  if (!name) return
+  Cookies.set(name, value, options);
+}
+
+/**
+ * 获取cookie
+ */
+export const getCookie = (name, options = {}) => {
+  if (!name) return
+  Cookies.get(name, options);
+}
+
+/**
+ * 移除cookie
+ */
+export const removeCookie = (name, options = {}) => {
+  if (!name) return
+  Cookies.remove(name, options);
+}
+
 /**
  * 存储sessionStorage
  */
 export const setSessionStore = (name, content) => {
-    if (!name) return
-    if (typeof content !== 'string') {
-        content = JSON.stringify(content)
-    }
-    try {
-        window.sessionStorage.setItem(name, content)
-    } catch (error) {
-        console.warn('无痕模式下不支持存储，err:%s', error)
-        window.sessionStorage.removeItem(name)
-    }
+  if (!name) return
+  if (typeof content !== 'string') {
+    content = JSON.stringify(content)
+  }
+  try {
+    window.sessionStorage.setItem(name, content)
+  } catch (error) {
+    console.warn('无痕模式下不支持存储，err:%s', error)
+    window.sessionStorage.removeItem(name)
+  }
 }
 
 /**
  * 获取sessionStorage
  */
 export const getSessionStore = name => {
-    if (!name) return
-    return window.sessionStorage.getItem(name)
+  if (!name) return
+  return window.sessionStorage.getItem(name)
 }
 
 /**
  * 删除localStorage
  */
 export const removeSessionStore = name => {
-    if (!name) return
-    window.sessionStorage.removeItem(name)
+  if (!name) return
+  window.sessionStorage.removeItem(name)
 }
 
 /**
  * 存储localStorage
  */
 export const setLocalStore = (name, content) => {
-    if (!name) return
-    if (typeof content !== 'string') {
-        content = JSON.stringify(content)
-    }
-    try {
-        window.localStorage.setItem(name, content)
-    } catch (error) {
-        console.warn('无痕模式下不支持存储，err:%s', error)
-        window.localStorage.removeItem(name)
-    }
+  if (!name) return
+  if (typeof content !== 'string') {
+    content = JSON.stringify(content)
+  }
+  try {
+    window.localStorage.setItem(name, content)
+  } catch (error) {
+    console.warn('无痕模式下不支持存储，err:%s', error)
+    window.localStorage.removeItem(name)
+  }
 }
 
 /**
  * 获取localStorage
  */
 export const getLocalStore = name => {
-    if (!name) return
-    return window.localStorage.getItem(name)
+  if (!name) return
+  return window.localStorage.getItem(name)
 }
 
 /**
  * 删除localStorage
  */
 export const removeLocalStore = name => {
-    if (!name) return
-    window.localStorage.removeItem(name)
+  if (!name) return
+  window.localStorage.removeItem(name)
 }
